a48819913f66f198c8252e24cc74edf3351872a1
VtoCorleone <shane.chapman@live.com>
2015-11-10 12:03:20 -0800

    Initial Commit

---
+++ Checklists/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Shane Chapman on 11/10/15.
+//  Copyright © 2015 VtoCorleone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
